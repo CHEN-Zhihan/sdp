c77af129a93084c5636945497bf4eea3b8922c51
Zhihan CHEN <CHEN.Zhihan@outlook.com>
2016-10-26 13:53:48 +0800

    gg

--- general/urls.py
+++ general/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name="index"),
+    url(r'^(?P<participantID>[0-9]+)/$',views.participant,name='participant'),
+    url(r'^(?P<participantID>[0-9]+)/courses/$',views.courses,name='courses')
 ]
--- general/views.py
+++ general/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 # Create your views here.
+
+from .models import Course,Participant
+
 def index(request):
     return HttpResponse("Hello, this is View: general.index.")
+
+def participant(request,participantID):
+    participant = get_object_or_404(Participant,pk=participantID)
+    return render(request,'general/participant.html',{'participant':participant})
+    return HttpResponse("You're looking at participant {}".format(participant.name))
+def courses(request,participantID):
+    return HttpResponse("You're looking at participant {}'s courses".format(participantID))
