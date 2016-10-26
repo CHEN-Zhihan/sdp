8322cad7305040c322998bc8022284f0f981b7c9
Zhihan CHEN <CHEN.Zhihan@outlook.com>
2016-10-26 12:27:50 +0800

    setup admin and database

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Course,Category,Component,Instructor,Participant
+from .models import Module,TakenCourse
+
+admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(Component)
+admin.site.register(Instructor)
+admin.site.register(Participant)
+admin.site.register(Module)
+admin.site.register(TakenCourse)
--- general/models.py
+++ general/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+class Instructor(models.Model):
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    instructor = models.ForeignKey(Instructor,on_delete=models.CASCADE)
+    isOpen = models.BooleanField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
@@ -19,7 +33,25 @@ class Component(models.Model):
     index = models.IntegerField()
     module = models.ForeignKey(Module,on_delete=models.CASCADE)
     typeName = models.CharField(max_length=200)
-
+    content = models.FileField()
     def __str__(self):
         string = "{}-th of {}:{}".format(self.index,self.module.course.name,self.module.name)
         return string
+
+class Participant(models.Model):
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    currentCourse = models.ForeignKey(Course,null=True,blank=True,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class TakenCourse(models.Model):
+    completionDate = models.DateField()
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant,on_delete=models.CASCADE)
+
+    def __str__(self):
+        participant = self.participant.name
+        course = self.course.name
+        date = str(self.completionDate)
+        return "{participant} completed {course} on {date}".format(participant=participant,course=course,date=date)
