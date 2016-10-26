9a8d0b4c25b324357bd6d8f82b48f515935c7b7d
WANG Zixu <wangzixu.china@gmail.com>
2016-10-26 18:02:27 +0800

    Build LESS/CSS env. Build basic stylesheets.

--- general/urls.py
+++ general/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name="index"),
     url(r'^(?P<participantID>[0-9]+)/$',views.participant,name='participant'),
-    url(r'^(?P<participantID>[0-9]+)/courses/(?P<courseID>[0-9]+)$',views.courses,name='courses')
-    url(r'^(?P<participantID>[0-9]+)/courses/(?P<courseID>[0-9]+)/enroll/$')
+    url(r'^(?P<participantID>[0-9]+)/courses/(?P<courseID>[0-9]+)$',views.courses,name='courses'),
 ]
