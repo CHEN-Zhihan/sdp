27466ed28f380b7e33002e1bbea008f69dc37a95
WANG Zixu <wangzixu.china@gmail.com>
2016-11-01 21:12:38 +0800

    Implement course page and enrollment.

--- general/static/js/common.js
+++ general/static/js/common.js
@@ -1,22 +1,30 @@
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+function getCookie (name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+function csrfSafeMethod (method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function redirectHome() {
+  var protocol = window.location.protocol;
+  var host = window.location.host;
+  var pathArray = window.location.pathname.split("/");
+  var newPath = pathArray[1] + "/" + pathArray[2];
+  window.location.assign(protocol + "//" + host + "/" + newPath);
 }
 
 $(document).ready(function () {
@@ -31,11 +39,7 @@ $(document).ready(function () {
 
   // Redirect to home page when Home is clicked
   $("#home").click(function () {
-    var protocol = window.location.protocol;
-    var host = window.location.host;
-    var pathArray = window.location.pathname.split("/");
-    var newPath = pathArray[1] + "/" + pathArray[2];
-    window.location.assign(protocol + "//" + host + "/" + newPath);
+    redirectHome();
   });
 
   // Highlight clicked anchor
