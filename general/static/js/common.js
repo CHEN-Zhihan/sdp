ce3574bdc2fc1f40447cb23456e120118e42db0b
WANG Zixu <wangzixu.china@gmail.com>
2016-11-01 16:12:32 +0800

    Integrate front-end and back-end. Fix bug with home button.

--- general/static/js/common.js
+++ general/static/js/common.js
@@ -7,9 +7,12 @@ $(document).ready(function () {
   // Redirect to home page when Home is clicked
   $("#home").click(function () {
     var protocol = window.location.protocol;
+    console.log(protocol);
     var host = window.location.host;
+    console.log(host);
     var pathArray = window.location.pathname.split("/");
-    var newPath = pathArray[0] + "/" + pathArray[1];
+    var newPath = pathArray[1] + "/" + pathArray[2];
+    console.log(newPath);
     window.location.assign(protocol + "//" + host + "/" + newPath);
   });
 
