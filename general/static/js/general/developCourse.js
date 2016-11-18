0b3e0ec2be3c2bf31e4ae27070ee0ffe4d03880c
Layton <wangzixu.china@gmail.com>
2016-11-19 00:59:05 +0800

    Implement open and delete course in front end.

--- general/static/js/general/developCourse.js
+++ general/static/js/general/developCourse.js
@@ -15,6 +15,76 @@ function registerAddModuleListener() {
   });
 }
 
+// Respond to open a course
+function registerOpenListener() {
+  $(".btn-open").click(function () {
+    console.log("Choose to open");
+    $(".btn-confirm", "#openConfirmModal").click(function () {
+      // Ajax POST
+      $.ajax({
+        url     : window.location.pathname,
+        type    : "POST",
+        data    : {"action": "OPEN"},
+        success : function (response) {
+          // Prompt result
+          console.log(response);
+          if (response['result']) {
+            $(".btn-success", "#openConfirmModal").click(function () {
+              window.location.reload(true);
+            });
+            $("#openSuccessModal").modal();
+          } else {
+            // On error, prompt enrollment error
+            $("#openFailModal").modal();
+          }
+        },
+        error   : function (XMLHttpRequest, textStatus, errorThrown) {
+          // On error, log the error info and prompt through error modal
+          console.log(XMLHttpRequest.readyState + XMLHttpRequest.status + XMLHttpRequest.responseText);
+          $("#errorModal").modal();
+        }
+      });
+    });
+    $("#openConfirmModal").modal();
+  });
+}
+
+// Respond to delete a course
+function registerDeleteListener() {
+  $(".btn-delete").click(function () {
+    console.log("Choose to open");
+    $(".btn-confirm", "#deleteConfirmModal").click(function () {
+      // Ajax POST
+      $.ajax({
+        url     : window.location.pathname,
+        type    : "POST",
+        data    : {"action": "DELETE"},
+        success : function (response) {
+          // Prompt result
+          console.log(response);
+          if (response['result']) {
+            $(".btn-success", "#deleteSuccessModal").click(function () {
+              redirectHome();
+            });
+            $("#deleteSuccessModal").modal();
+          } else {
+            // On error, prompt enrollment error
+            $("#deleteFailModal").modal();
+          }
+        },
+        error   : function (XMLHttpRequest, textStatus, errorThrown) {
+          // On error, log the error info and prompt through error modal
+          console.log(XMLHttpRequest.readyState + XMLHttpRequest.status + XMLHttpRequest.responseText);
+          $("#errorModal").modal();
+        }
+      });
+    });
+    $("#deleteConfirmModal").modal();
+  });
+}
+
 $(document).ready(function () {
   registerAddModuleListener();
+  registerOpenListener();
+  registerDeleteListener();
 });
