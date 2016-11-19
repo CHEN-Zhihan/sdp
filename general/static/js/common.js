674134f2eb9ce708a0e3f5800b3d18d95dc0b205
WANG Zixu <wangzixu.china@gmail.com>
2016-11-20 04:32:44 +0800

    Fix category listener.

--- general/static/js/common.js
+++ general/static/js/common.js
@@ -83,6 +83,24 @@ function registerModuleListener() {
   });
 }
 
+// Respond to a category clicked
+function registerCategoryListener() {
+  $(".category").click(function () {
+    // Get category name and ID
+    var categoryName = $(this).text();
+    var categoryID = parseInt($(this).attr("id"));
+    console.log("Category " + categoryName + " selected");
+    var protocol = window.location.protocol;
+    var host = window.location.host;
+    var pathArray = window.location.pathname.split("/");
+    var newPath = pathArray[1] + "/" + pathArray[2]
+                  + "/showCourseList?categoryID=" + categoryID;
+
+    // Redirect to new URL
+    window.location.assign(protocol + "//" + host + "/" + newPath);
+  });
+}
+
 $(document).ready(function () {
   // Set up AJAX for Django CSRF token
   $.ajaxSetup({
