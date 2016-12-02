2fb5eebb2a2ba80978c58dd972fe01d99830c05e
Layton Wang <wangzixu.china@gmail.com>
2016-12-02 21:03:14 +0800

    Ensure CSRF in login page.

--- general/static/js/general/login.js
+++ general/static/js/general/login.js
@@ -5,70 +5,68 @@ function registerTabListener() {
     console.log($(this).text().trim());
     if ($(this).text().trim() == "LOGIN") {
       $(".form-container").html(`
-        <form id="loginForm">
-          <input name="action" value="LOGIN" hidden/>
-          <!-- Username -->
-          <div class="form-group">
-            <input type="text" class="well well-sm well-input" placeholder="Username" name="username"></input>
-          </div>
+        <input name="action" value="LOGIN" hidden/>
+        <!-- Username -->
+        <div class="form-group">
+          <input type="text" class="well well-sm well-input" placeholder="Username" name="username"></input>
+        </div>
 
-          <!-- Password -->
-          <div class="form-group">
-            <input type="password" class="well well-sm well-input" placeholder="Password" name="password" id="password"></input>
-          </div>
+        <!-- Password -->
+        <div class="form-group">
+          <input type="password" class="well well-sm well-input" placeholder="Password" name="password" id="password"></input>
+        </div>
 
-          <!-- Select user type -->
-          <div class="form-group">
-            <select class="well well-sm well-input" name="usertype">
-              <option value="Participant">Participant</option>
-              <option value="Instructor">Instructor</option>
-              <option value="HR">HR</option>
-              <option value="Administrator">Administrator</option>
-            </select>
-          </div>
+        <!-- Select user type -->
+        <div class="form-group">
+          <select class="well well-sm well-input" name="usertype">
+            <option value="Participant">Participant</option>
+            <option value="Instructor">Instructor</option>
+            <option value="HR">HR</option>
+            <option value="Administrator">Administrator</option>
+          </select>
+        </div>
 
-          <!-- Submit button -->
-          <div class="form-group submit">
-            <button type="submit" class="btn btn-default">Login</button>
-          </div>
-        </form>
+        <!-- Submit button -->
+        <div class="form-group submit">
+          <button type="submit" class="btn btn-default">Login</button>
+        </div>
       `);
+      $("form").attr("id", "loginForm").off("submit");
       registerLoginListener();
     } else if ($(this).text().trim() == "SIGN UP") {
       $(".form-container").html(`
-        <form id="registerForm">
-          <input name="action" value="REGISTER" hidden/>
-          <!-- Username -->
-          <div class="form-group">
-            <input type="text" class="well well-sm well-input" placeholder="Username" name="username" id="username"></input>
-          </div>
+        <input name="action" value="REGISTER" hidden/>
+        <!-- Username -->
+        <div class="form-group">
+          <input type="text" class="well well-sm well-input" placeholder="Username" name="username" id="username"></input>
+        </div>
 
-          <!-- First name -->
-          <div class="form-group">
-            <input type="text" class="well well-sm well-input" placeholder="First name" name="firstName"></input>
-          </div>
+        <!-- First name -->
+        <div class="form-group">
+          <input type="text" class="well well-sm well-input" placeholder="First name" name="firstName"></input>
+        </div>
 
-          <!-- Last name -->
-          <div class="form-group">
-            <input type="text" class="well well-sm well-input" placeholder="Last name" name="lastName"></input>
-          </div>
+        <!-- Last name -->
+        <div class="form-group">
+          <input type="text" class="well well-sm well-input" placeholder="Last name" name="lastName"></input>
+        </div>
 
-          <!-- Password -->
-          <div class="form-group">
-            <input type="password" class="well well-sm well-input" placeholder="Password" name="password" id="password"></input>
-          </div>
+        <!-- Password -->
+        <div class="form-group">
+          <input type="password" class="well well-sm well-input" placeholder="Password" name="password" id="password"></input>
+        </div>
 
-          <!-- Confirm password -->
-          <div class="form-group">
-            <input type="password" class="well well-sm well-input" placeholder="Confirm password" id="confirm-password"></input>
-          </div>
+        <!-- Confirm password -->
+        <div class="form-group">
+          <input type="password" class="well well-sm well-input" placeholder="Confirm password" id="confirm-password"></input>
+        </div>
 
-          <!-- Submit button -->
-          <div class="form-group submit">
-            <button type="submit" class="btn btn-default">Sign up</button>
-          </div>
-        </form>
+        <!-- Submit button -->
+        <div class="form-group submit">
+          <button type="submit" class="btn btn-default">Sign up</button>
+        </div>
       `);
+      $("form").attr("id", "registerForm").off("submit");
       registerRegisterListener();
     }
   });
