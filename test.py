241a57703114879c70b1eee72587815a4d8eda2f
Zhihan CHEN <CHEN.Zhihan@outlook.com>
2016-12-04 13:27:07 +0800

    delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from general.models import Participant,Instructor,Category,Course
-
-
-categories = ["Information Technology","Mergers and Acquisitions", "Markets","Risk Management","Securities","Financial Modelling","Operations"]
-
-for c in categories:
-    if not Category.objects.filter(name=c).exists():
-        Category.create(c)
